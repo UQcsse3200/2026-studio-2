84bdf376eff93235760c26264a31a742dda0acc1
Lachlan-Holliday <lachlanhollidaymedia@gmail.com>
2026-08-23 10:32:48 +1000

    #ADD tests for non active minigames and back button

---
+++ source/core/src/test/com/csse3200/game/components/minigames/MinigameSelectActionsTest.java
@@ -0,0 +1,44 @@
+package com.csse3200.game.components.minigames;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+
+import com.csse3200.game.GdxGame;
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.extensions.GameExtension;
+import com.csse3200.game.screens.minigames.MinigameType;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(GameExtension.class)
+class MinigameSelectActionsTest {
+
+  /**
+   * Back button on minigame screen properly disposes and returns to main screen
+   */
+  @Test
+  void shouldReturnToMainMenuOnBack() {
+    GdxGame game = mock(GdxGame.class);
+    Entity ui = new Entity().addComponent(new MinigameSelectActions(game));
+    ui.create();
+
+    ui.getEvents().trigger("back");
+
+    verify(game).setScreen(GdxGame.ScreenType.MAIN_MENU);
+  }
+
+  /**
+   * If minigames are not playable yet, selecting one must not change screen.
+   */
+  @Test
+  void shouldNotChangeScreenForUnimplementedMinigame() {
+    GdxGame game = mock(GdxGame.class);
+    Entity ui = new Entity().addComponent(new MinigameSelectActions(game));
+    ui.create();
+
+    ui.getEvents().trigger("selectMinigame", MinigameType.SPIN_THE_WHEEL);
+
+    verifyNoInteractions(game);
+  }
+}
