df644e6c44e5645a3121fee35a9fd15a91904198
06wtkelly <06willtk25@gmail.com>
2026-08-26 14:22:37 +1000

    Updated preset values for bar data to be changable. Added stopping and starting the marker. Added simple docstrings

--- source/core/src/main/com/csse3200/game/components/minigames/CyclopsTimingBar/TimingBarLogic.java
+++ source/core/src/main/com/csse3200/game/components/minigames/CyclopsTimingBar/TimingBarLogic.java
@@ -1,51 +1,80 @@
 package com.csse3200.game.components.minigames.CyclopsTimingBar;
 
 /**
- * Mathematical state of the components in the timing bar.
- * Keeps track of size and location of scoring bar.
- * Keeps track of the location of the sweeping marker.
+ * Mathematical state of the components in the timing bar. Keeps track of size and location of
+ * scoring bar. Keeps track of the location of the sweeping marker.
  */
 public class TimingBarLogic {
 
-    private float barStart = 2f;
-    private float barWidth = 16f;
-    private float greenStart = 10f;
-    private float greenEnd = 13f;
+  public float barStart = 2f;
+  public float barWidth;
+  public float greenStart;
+  public float greenEnd;
 
-    private float markerX = 2f;
-    private float markerSpeed = 8f;
-    private int direction = 1;
+  public float markerX = barStart;
+  private final float markerSpeed = 10f;
+  private int direction = 1;
 
-    public void changeScoringAreaSize(float width) {
-        float barCenter = barWidth / 2;
+  public boolean isStopped = false;
 
-        this.greenStart = barCenter - (width / 2);
-        this.greenEnd = barCenter + (width / 2);
+  public TimingBarLogic(float barWidth, float scoringAreaWidth) {
+    this.barWidth = barWidth;
+    this.changeScoringAreaWidth(scoringAreaWidth);
+  }
+
+  /**
+   * Change the width of the scoring area to the given width
+   * @param width - float value of the new width of the scoring area
+   */
+  public void changeScoringAreaWidth(float width) {
+    float barCenter = barWidth / 2;
+
+    this.greenStart = barCenter - (width / 2);
+    this.greenEnd = barCenter + (width / 2);
+  }
+
+  /**
+   * Update the marker to move along the bar.
+   *
+   * @param deltaTime
+   */
+  public void update(float deltaTime) {
+    if (isStopped) {
+      return;
     }
 
-    /**
-     * Update the marker to move along the bar.
-     * @param deltaTime
-     */
-    public void update(float deltaTime) {
-        markerX += markerSpeed * direction * deltaTime;
+    markerX += markerSpeed * direction * deltaTime;
 
-        float rightEdge = barStart + barWidth;
-        if (markerX >= rightEdge) {
-            markerX = rightEdge;
-            direction = -1;
-        } else if (markerX <= barStart) {
-            markerX = barStart;
-            direction = 1;
-        }
+    float rightEdge = barStart + barWidth;
+    if (markerX >= rightEdge) {
+      markerX = rightEdge;
+      direction = -1;
+    } else if (markerX <= barStart) {
+      markerX = barStart;
+      direction = 1;
     }
+  }
 
-    /**
-     * Checks whether the marker is in the scoring green area.
-     * @return True when the marker is in the scoring area, otherwise False.
-     */
-    public boolean checkHit() {
-        return (markerX >= greenStart && markerX <= greenEnd);
-    }
+  /**
+   * Stops the sliding marker
+   */
+  public void stopMarker() {
+    this.isStopped = true;
+  }
 
+  /**
+   * Starts the sliding marker
+   */
+  public void startMarker() {
+    this.isStopped = false;
+  }
+
+  /**
+   * Checks whether the marker is in the scoring green area.
+   *
+   * @return True when the marker is in the scoring area, otherwise False.
+   */
+  public boolean checkHit() {
+    return (markerX >= greenStart && markerX <= greenEnd);
+  }
 }
