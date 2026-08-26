c1d3c87094ec6b27ddd65b1f39ff6ab5fd3d4121
06wtkelly <06willtk25@gmail.com>
2026-08-26 21:50:39 +1000

    Revert "Fixed bug minor error where timing bar wouldn't fully on player input. Added temporary dev tool to restart the timing bar."
    This reverts commit e7a1864e5f7141e67701538b0e055b707d5fe821.

--- source/core/src/main/com/csse3200/game/components/minigames/CyclopsTimingBar/CyclopsMinigameLogicComponent.java
+++ source/core/src/main/com/csse3200/game/components/minigames/CyclopsTimingBar/CyclopsMinigameLogicComponent.java
@@ -10,43 +10,22 @@ public class CyclopsMinigameLogicComponent extends Component {
   private static final Logger logger = LoggerFactory.getLogger(CyclopsMinigameLogicComponent.class);
 
   private final TimingBarLogic logic;
-  private final TimingBarDisplay display;
 
-  private boolean running;
-
-  public CyclopsMinigameLogicComponent(TimingBarLogic logic, TimingBarDisplay display) {
+  public CyclopsMinigameLogicComponent(TimingBarLogic logic) {
     this.logic = logic;
-    this.display = display;
-    this.display.setVisible(false);
-    this.logic.stopMarker();
-
-    this.running = false;
   }
 
   @Override
   public void update() {
-    if (running) {
-      // Check input and do stuff with timingBar etc
-      if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
-        logger.info("sliding marker stopped");
-        logic.stopMarker();
-        running = false;
-      }
+    // Check input and do stuff with timingBar etc
+    if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+      logic.stopMarker();
+    }
 
-      if (logic != null && !logic.isStopped) {
-        logic.update(Gdx.graphics.getDeltaTime());
-      }
-    } else if (Gdx.input.isKeyPressed(Input.Keys.PERIOD)) { // DEV TOOL Sort of
-      logger.info("DEV: activated timing bar");
-      logic.startMarker();
-      running = true;
+    if (logic != null && !logic.isStopped) {
+      logic.update(Gdx.graphics.getDeltaTime());
     }
   }
 
-  public void startMinigame() {
-    logger.info("starting timing bar minigame");
-    this.running = true;
-    this.display.setVisible(true);
-    this.logic.startMarker();
-  }
+  public void startMinigame() {}
 }
