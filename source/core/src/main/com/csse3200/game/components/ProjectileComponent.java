5ff11acd42fe3eaaa59407be33cb08980598ba3d
ZarifHilman <zarifhilman02@gmail.com>
2026-08-27 13:32:04 +1000

    Add ranged enemy projectile attack

---
+++ source/core/src/main/com/csse3200/game/components/ProjectileComponent.java
@@ -0,0 +1,26 @@
+package com.csse3200.game.components;
+
+import com.csse3200.game.services.ServiceLocator;
+
+public class ProjectileComponent extends Component {
+
+  private float remainingLifetime;
+
+  /**
+   * Creates a projectile component.
+   *
+   * @param lifetime maximum lifetime of the projectile in seconds
+   */
+  public ProjectileComponent(float lifetime) {
+    this.remainingLifetime = lifetime;
+  }
+
+  @Override
+  public void update() {
+    remainingLifetime -= ServiceLocator.getTimeSource().getDeltaTime();
+
+    if (remainingLifetime <= 0f) {
+      entity.dispose();
+    }
+  }
+}
