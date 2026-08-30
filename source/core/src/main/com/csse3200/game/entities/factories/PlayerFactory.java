4c5abb0df78b9a206874c8744d8e5c8719a4a6fe
TristanUniCoding <t.sheedy@student.uq.edu.au>
2026-08-30 18:56:42 +1000

    Fixing sprite framing - stage 1

--- source/core/src/main/com/csse3200/game/entities/factories/PlayerFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/PlayerFactory.java
@@ -62,6 +62,7 @@ public class PlayerFactory {
     PhysicsUtils.setScaledCollider(player, 0.6f, 0.3f);
     player.getComponent(ColliderComponent.class).setDensity(1.5f);
     player.getComponent(AnimationRenderComponent.class).scaleEntity();
+    player.setScale(1.5f, 1.5f);
     return player;
   }
 
