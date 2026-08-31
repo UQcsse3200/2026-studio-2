395a331758668a70615323d2245889f6d0be9fcf
s45855377 <c.somerfield@student.uq.edu.au>
2026-08-31 12:01:46 +1000

    Changed GameArea to include a list of all platforms within that location and added methods for finding the closest platform to a point and checking for successful grapples

--- source/core/src/main/com/csse3200/game/areas/GameArea.java
+++ source/core/src/main/com/csse3200/game/areas/GameArea.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.math.GridPoint2;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Disposable;
 import com.csse3200.game.areas.terrain.TerrainComponent;
+import com.csse3200.game.components.level.PlatformGrappleComponent;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.services.ServiceLocator;
 import java.util.ArrayList;
@@ -18,6 +19,7 @@ import java.util.List;
 public abstract class GameArea implements Disposable {
   protected TerrainComponent terrain;
   protected List<Entity> areaEntities;
+  protected List<Entity> platforms = new ArrayList<>();
 
   protected GameArea() {
     areaEntities = new ArrayList<>();
@@ -40,6 +42,12 @@ public abstract class GameArea implements Disposable {
    */
   protected void spawnEntity(Entity entity) {
     areaEntities.add(entity);
+
+    // keep track of all grappleable platforms
+    if (entity.getComponent(PlatformGrappleComponent.class) != null) {
+      platforms.add(entity);
+    }
+
     ServiceLocator.getEntityService().register(entity);
   }
 
@@ -65,5 +73,50 @@ public abstract class GameArea implements Disposable {
 
     entity.setPosition(worldPos);
     spawnEntity(entity);
+  }
+
+  /**
+   * A protected method that loops through all stored platforms
+   *
+   * @param raycastEnd the Vector2 object created by the physics engine's raycast that corresponds
+   *     to the final point in the world the grapple hit
+   * @return An entity that is guaranteed to be a platform entity (the platform check is performed
+   *     before adding to the platforms list) that is the closest to the point at the end of the
+   *     raycast.<br>
+   *     Note: this does not guarantee the platform was successfully hit, it just returns the most
+   *     likely entity to do the check on
+   */
+  protected Entity findTargetedPlatform(Vector2 raycastEnd) {
+    Entity platform = null;
+    float lowestDistance = Float.MAX_VALUE; // ensures platforms[0] will be best candidate initially
+
+    for (Entity entity : platforms) {
+      // find distance between this and raycast end and compare to best known candidate
+      Vector2 pos = entity.getCenterPosition();
+      float currentDistance = pos.dst(raycastEnd);
+
+      // update best candidate
+      if (currentDistance < lowestDistance) {
+        platform = entity;
+        lowestDistance = currentDistance;
+      }
+    }
+    return platform;
+  }
+
+  /**
+   * Public method for grapples to check the end of the raycast position hits a valid side of a
+   * platform to confirm a successful grapple location was hit
+   *
+   * @param raycastEnd the Vector2 object created by the physics engine's raycast that corresponds
+   *     to the final point in the world the grapple hit
+   * @return true if the grapple successfully hit a platform and the hit side was a valid,
+   *     grappleable side of that platform, false otherwise
+   */
+  public boolean checkSuccessfulGrapple(Vector2 raycastEnd) {
+    Entity p = findTargetedPlatform(raycastEnd);
+    PlatformGrappleComponent grappleComponent = p.getComponent(PlatformGrappleComponent.class);
+    int hit = grappleComponent.checkSideHit(p, raycastEnd);
+    return grappleComponent.successfulGrapple(hit);
   }
 }
