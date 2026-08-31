a185f6586237670050d2acde6df3b46149f3f9b1
s45855377 <c.somerfield@student.uq.edu.au>
2026-08-31 17:58:20 +1000

    spotlessApply fixes

--- source/core/src/main/com/csse3200/game/areas/TutorialGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/TutorialGameArea.java
@@ -42,7 +42,6 @@ public class TutorialGameArea extends GameArea {
     new PlatformConfig(new GridPoint2(9, 13), 3, 1, 0)
   };
 
-
   private static final PlatformConfig[] floors = {
     new PlatformConfig(new GridPoint2(0, 0), 3, 3, 0),
     new PlatformConfig(new GridPoint2(13, 0), 12, 5, 0),
@@ -53,7 +52,6 @@ public class TutorialGameArea extends GameArea {
     new PlatformConfig(new GridPoint2(0, 21), 17, 3, 0),
     new PlatformConfig(new GridPoint2(28, 19), 4, 1, 0),
     new PlatformConfig(new GridPoint2(35, 21), 15, 5, 0)
-
   };
 
   private static final GridPoint2 PLAYER_SPAWN = new GridPoint2(10, 10);
