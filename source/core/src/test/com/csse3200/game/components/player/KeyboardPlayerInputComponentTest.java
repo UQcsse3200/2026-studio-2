9db3958d2509740bd19c2bcf971d7cf35ae70802
DarcyMF <DarcyMFranklin@gmail.com>
2026-09-01 12:49:46 +1000

    refactor: modified keyboardplayerinputcomponenttest to adhere to the new weapon controls

--- source/core/src/test/com/csse3200/game/components/player/KeyboardPlayerInputComponentTest.java
+++ source/core/src/test/com/csse3200/game/components/player/KeyboardPlayerInputComponentTest.java
@@ -25,13 +25,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(GameExtension.class)
 class KeyboardPlayerInputComponentTest {
-  private Input input;
   private Camera camera;
 
   @BeforeEach
   void setUp() {
-    input = mock(Input.class);
-    Gdx.input = input;
+    Gdx.input = mock(Input.class);
     camera = mock(Camera.class);
     when(camera.unproject(any(Vector3.class)))
         .thenAnswer(
@@ -41,12 +39,30 @@ class KeyboardPlayerInputComponentTest {
             });
   }
 
-  @Test
-  void shouldFireTowardClickedWorldPosition() {
+  private KeyboardPlayerInputComponent aimedComponent(Entity player) {
     KeyboardPlayerInputComponent component = new KeyboardPlayerInputComponent();
-    Entity player = new Entity().addComponent(component);
+    player.addComponent(component);
     player.setPosition(0f, 0f);
     component.setCameraComponent(new CameraComponent(camera));
+    return component;
+  }
+
+  @Test
+  void shouldMeleeOnLeftClick() {
+    Entity player = new Entity();
+    KeyboardPlayerInputComponent component = aimedComponent(player);
+
+    AtomicReference<Vector2> direction = new AtomicReference<>();
+    player.getEvents().addListener("melee", (Vector2 aim) -> direction.set(aim));
+
+    assertTrue(component.touchDown(4, 2, 0, Buttons.LEFT));
+    assertTrue(direction.get().epsilonEquals(new Vector2(9.5f, 4.5f)));
+  }
+
+  @Test
+  void shouldShootOnRightClick() {
+    Entity player = new Entity();
+    KeyboardPlayerInputComponent component = aimedComponent(player);
 
     AtomicInteger shots = new AtomicInteger();
     AtomicReference<Vector2> direction = new AtomicReference<>();
@@ -54,18 +70,26 @@ class KeyboardPlayerInputComponentTest {
         .getEvents()
         .addListener(
             "shoot",
-            (Vector2 aimDirection) -> {
+            (Vector2 aim) -> {
               shots.incrementAndGet();
-              direction.set(aimDirection);
+              direction.set(aim);
             });
 
-    assertTrue(component.touchDown(4, 2, 0, Buttons.LEFT));
+    assertTrue(component.touchDown(4, 2, 0, Buttons.RIGHT));
     assertEquals(1, shots.get());
     assertTrue(direction.get().epsilonEquals(new Vector2(9.5f, 4.5f)));
+  }
 
-    assertTrue(component.touchUp(4, 2, 0, Buttons.LEFT));
-    assertTrue(component.touchDown(4, 2, 0, Buttons.LEFT));
-    assertEquals(2, shots.get());
+  @Test
+  void shouldSignalStopShootOnRightRelease() {
+    Entity player = new Entity();
+    KeyboardPlayerInputComponent component = aimedComponent(player);
+
+    AtomicInteger stops = new AtomicInteger();
+    player.getEvents().addListener("stopShoot", stops::incrementAndGet);
+
+    assertTrue(component.touchUp(4, 2, 0, Buttons.RIGHT));
+    assertEquals(1, stops.get());
   }
 
   @Test
@@ -75,8 +99,20 @@ class KeyboardPlayerInputComponentTest {
     AtomicInteger shots = new AtomicInteger();
     player.getEvents().addListener("shoot", (Vector2 ignored) -> shots.incrementAndGet());
 
-    assertFalse(component.touchDown(4, 2, 0, Buttons.LEFT));
+    assertFalse(component.touchDown(4, 2, 0, Buttons.RIGHT));
     assertEquals(0, shots.get());
+  }
+
+  @Test
+  void shouldCycleArrowOnQ() {
+    Entity player = new Entity();
+    KeyboardPlayerInputComponent component = aimedComponent(player);
+
+    AtomicInteger cycles = new AtomicInteger();
+    player.getEvents().addListener("cycleArrow", cycles::incrementAndGet);
+
+    assertTrue(component.keyDown(Keys.Q));
+    assertEquals(1, cycles.get());
   }
 
   @Test
@@ -111,19 +147,5 @@ class KeyboardPlayerInputComponentTest {
     assertEquals(1, sprints.get());
     assertTrue(component.keyUp(Keys.SHIFT_LEFT));
     assertEquals(1, sprintStops.get());
-  }
-
-  @Test
-  void shouldShootSelectedArrowTowardClickedWorldPosition() {
-    KeyboardPlayerInputComponent component = new KeyboardPlayerInputComponent();
-    Entity player = new Entity().addComponent(component);
-    player.setPosition(0f, 0f);
-    component.setCameraComponent(new CameraComponent(camera));
-    AtomicReference<Vector2> direction = new AtomicReference<>();
-    player.getEvents().addListener("shoot", (Vector2 aim) -> direction.set(aim));
-
-    assertFalse(component.touchDown(4, 2, 0, Buttons.RIGHT));
-    assertTrue(component.touchDown(4, 2, 0, Buttons.LEFT));
-    assertTrue(direction.get().epsilonEquals(new Vector2(9.5f, 4.5f)));
   }
 }
