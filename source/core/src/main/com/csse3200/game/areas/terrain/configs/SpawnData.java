96319d018643631d6a71b9d7b5bd17b34ddc8efb
s45855377 <c.somerfield@student.uq.edu.au>
2026-09-09 08:30:00 +1000

    Added a spawn data object that stores an entity and a position to spawn that entity in to be used by LevelConfig

---
+++ source/core/src/main/com/csse3200/game/areas/terrain/configs/SpawnData.java
@@ -0,0 +1,19 @@
+package com.csse3200.game.areas.terrain.configs;
+
+import com.badlogic.gdx.math.GridPoint2;
+import com.csse3200.game.entities.Entity;
+
+public class SpawnData {
+  public GridPoint2 pos;
+  public Entity entity;
+
+    /**
+     * Creates new SpawnData object used to spawn an entity in a position in a level
+     * @param pos Position to spawn the entity at
+     * @param entity Entity to spawn at specified position
+     */
+  public SpawnData(GridPoint2 pos, Entity entity) {
+    this.pos = pos;
+    this.entity = entity;
+  }
+}
