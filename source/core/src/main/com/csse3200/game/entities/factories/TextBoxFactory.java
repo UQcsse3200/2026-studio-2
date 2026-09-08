101b14ef321648e184abad1596b7be19520eab9b
Kinsey Dodd <kinseydodd317@gmail.com>
2026-09-08 18:41:23 +1000

    Testing stuff

--- source/core/src/main/com/csse3200/game/entities/factories/TextBoxFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/TextBoxFactory.java
@@ -6,34 +6,32 @@ import com.csse3200.game.entities.configs.TextConfig;
 import com.csse3200.game.files.FileLoader;
 
 public class TextBoxFactory {
+  public static final TextConfig textConfig =
+      FileLoader.readClass(TextConfig.class, "configs/textBoxes.json");
 
   /** Takes in an already created TextConfig file and extracts the parameters */
-  public TextBoxFactory() {}
+  public TextBoxFactory() {
+    // this.textConfig = FileLoader.readClass(TextConfig.class, "configs/textBoxes.json");
+  }
 
   /**
    * @param filePath the path from source. e.g: configs/textBoxes.json
    */
-  public static void createTextBox(String filePath) {
+  public void createTextBox(String filePath) {
     try {
-      TextConfig config = FileLoader.readClass(TextConfig.class, filePath);
-      if (config == null) {
-        System.err.println("Error: Could not load TextConfig from " + filePath);
-        return;
-      }
-
       new TextBoxComponent(
-          config.xPos,
-          config.yPos,
+          textConfig.xPos,
+          textConfig.yPos,
           Color.WHITE,
           Color.BLACK,
           Color.WHITE,
-          config.charsPerSecond,
-          config.maxWidth,
-          config.padding,
-          config.borderThickness,
-          config.text);
+          textConfig.charsPerSecond,
+          textConfig.maxWidth,
+          textConfig.padding,
+          textConfig.borderThickness,
+          textConfig.text).create();
     } catch (Exception e) {
-      e.printStackTrace();
+      System.out.println("Error: " + e.getMessage());
     }
   }
 }
