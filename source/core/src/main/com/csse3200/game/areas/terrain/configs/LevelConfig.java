5bff03e79543f537cbf776288f45fa7de3486df2
s45855377 <c.somerfield@student.uq.edu.au>
2026-09-09 08:29:15 +1000

    Added a new level config class that is intended for level config files to be created and extend in order to specify the entities required for that level

---
+++ source/core/src/main/com/csse3200/game/areas/terrain/configs/LevelConfig.java
@@ -0,0 +1,145 @@
+package com.csse3200.game.areas.terrain.configs;
+
+import com.badlogic.gdx.math.GridPoint2;
+import com.csse3200.game.components.item.Item;
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.entities.configs.EnemyConfig;
+import com.csse3200.game.entities.factories.ItemFactory;
+import com.csse3200.game.entities.factories.ObstacleFactory;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class LevelConfig {
+  protected PlatformConfig[] platforms;
+  protected MovingPlatformConfig[] movingPlatforms;
+  protected CrumblingPlatformConfig[] crumblingPlatforms;
+  protected TriggerablePlatformConfig[] triggerablePlatforms;
+  protected SpikeClusterConfig[] spikes;
+  protected PlatformConfig[] bounds;
+  protected PlatformConfig[] floors;
+  protected Map<GridPoint2, EnemyConfig> enemies;
+  protected Map<GridPoint2, Item> items;
+
+  protected GridPoint2 playerSpawn;
+  protected GridPoint2 winConditionSpawn;
+
+  protected ArrayList<SpawnData> entities = new ArrayList<>();
+
+  /**
+   * Creates all entities as specified by the config file and returns the created entities in a Map
+   * for the level to spawn the objects at
+   *
+   * @return A list of SpawnData objects that contain an entity and a coordinate position to spawn
+   *     that entity in
+   */
+  public ArrayList<SpawnData> createEntities() {
+    entities = new ArrayList<>();
+
+    createFloors();
+    createPlatforms();
+    createMovingPlatforms();
+    createCrumblingPlatforms();
+    createTriggerablePlatforms();
+    createSpikes();
+    createWinCondition();
+    createItems();
+
+    return entities;
+  }
+
+  /**
+   * Public getter for the player spawn for a level config
+   *
+   * @return The spawn coordinates for the player stored by this config as a GridPoint2
+   */
+  public GridPoint2 getPlayerSpawn() {
+    return playerSpawn;
+  }
+
+  /**
+   * Creates all platforms for this level and adds them to the entities Map for the level to spawn
+   */
+  private void createPlatforms() {
+    for (PlatformConfig p : platforms) {
+      Entity platform = ObstacleFactory.createPlatform(p.grappleSides);
+      platform.setScale(p.width, p.height);
+      entities.add(new SpawnData(p.position, platform));
+    }
+  }
+
+  /**
+   * Creates all moving platforms for this level and adds them to the entities Map for the level to
+   * spawn
+   */
+  private void createMovingPlatforms() {
+    for (MovingPlatformConfig p : movingPlatforms) {
+      Entity platform =
+          ObstacleFactory.createMovingPlatform(
+              p.grappleSides, p.firstTarget, p.secondTarget, p.speed);
+      platform.setScale(p.width, p.height);
+      entities.add(new SpawnData(p.position, platform));
+    }
+  }
+
+  /**
+   * Creates all crumbling platforms for this level and adds them to the entities Map for the level
+   * to spawn
+   */
+  private void createCrumblingPlatforms() {
+    // stub
+  }
+
+  /**
+   * Creates all triggerable platforms and buttons associated with those platforms for this level
+   * and adds them to the entities Map for the level to spawn
+   */
+  private void createTriggerablePlatforms() {
+    // stub
+  }
+
+  /**
+   * Creates the bounds and floors for the level and adds them to the entities Map for the level to
+   * spawn
+   */
+  private void createFloors() {
+    for (PlatformConfig b : bounds) {
+      Entity bound = ObstacleFactory.createFloor(b.grappleSides);
+      bound.setScale(b.width, b.height);
+      entities.add(new SpawnData(b.position, bound));
+    }
+
+    for (PlatformConfig f : floors) {
+      Entity bound = ObstacleFactory.createFloor(f.grappleSides);
+      bound.setScale(f.width, f.height);
+      entities.add(new SpawnData(f.position, bound));
+    }
+  }
+
+  /** Creates all the spike clusters and adds them to the entities map for the level to spawn */
+  private void createSpikes() {
+    for (SpikeClusterConfig s : spikes) {
+      for (int i = s.xMin; i <= s.xMax; i++) {
+        for (int j = s.yMin; j <= s.yMax; j++) {
+          Entity spike = ObstacleFactory.createSpike();
+          entities.add(new SpawnData(new GridPoint2(i, j), spike));
+        }
+      }
+    }
+  }
+
+  /** Creates the win condition entity and adds it to the entities Map for the level to spawn */
+  private void createWinCondition() {
+    Entity winCon = ObstacleFactory.createWinConEntity();
+    entities.add(new SpawnData(winConditionSpawn, winCon));
+  }
+
+  /**
+   * Creates all items specified for the level and adds them to the entity tracker when requested
+   */
+  private void createItems() {
+    for (Map.Entry<GridPoint2, Item> i : items.entrySet()) {
+      Entity item = ItemFactory.createItem(i.getValue());
+      entities.add(new SpawnData(i.getKey(), item));
+    }
+  }
+}
