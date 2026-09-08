a9412f2bfdbf9873be0ce74238851fa4b98ea178
s45855377 <c.somerfield@student.uq.edu.au>
2026-09-09 08:32:00 +1000

    Removed all config that is now being handled by other classes and cleaned up deprecated methods

--- source/core/src/main/com/csse3200/game/areas/TutorialGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/TutorialGameArea.java
@@ -3,23 +3,20 @@ package com.csse3200.game.areas;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.math.GridPoint2;
 import com.badlogic.gdx.math.Vector2;
-import com.csse3200.game.areas.terrain.PlatformConfig;
 import com.csse3200.game.areas.terrain.TerrainFactory;
 import com.csse3200.game.areas.terrain.TerrainFactory.TerrainType;
+import com.csse3200.game.areas.terrain.configs.levelconfigs.LevelTutorialConfig;
 import com.csse3200.game.components.CameraComponent;
 import com.csse3200.game.components.gamearea.GameAreaDisplay;
 import com.csse3200.game.components.player.KeyboardPlayerInputComponent;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.entities.factories.EnemyFactory;
-import com.csse3200.game.entities.factories.ItemFactory;
-import com.csse3200.game.entities.factories.NPCFactory;
 import com.csse3200.game.entities.factories.ObstacleFactory;
 import com.csse3200.game.entities.factories.PlayerFactory;
 import com.csse3200.game.rendering.BackgroundRenderComponent;
 import com.csse3200.game.services.ResourceService;
 import com.csse3200.game.services.ServiceLocator;
 import com.csse3200.game.utils.math.GridPoint2Utils;
-import com.csse3200.game.utils.math.RandomUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,71 +24,6 @@ import org.slf4j.LoggerFactory;
 public class TutorialGameArea extends GameArea {
 
   private static final Logger logger = LoggerFactory.getLogger(TutorialGameArea.class);
-
-  private static final int NUM_TREES = 7;
-  private static final int NUM_GHOSTS = 2;
-
-  private static final PlatformConfig[] platforms = {
-    // first half
-    new PlatformConfig(new GridPoint2(4, 2), 3, 1, 0),
-    new PlatformConfig(new GridPoint2(8, 3), 3, 1, 0),
-    new PlatformConfig(new GridPoint2(14, 6), 3, 1, 0),
-    new PlatformConfig(new GridPoint2(19, 6), 3, 1, 0),
-    new PlatformConfig(new GridPoint2(27, 2), 3, 1, 0),
-    new PlatformConfig(new GridPoint2(32, 3), 2, 2, 2),
-    new PlatformConfig(new GridPoint2(30, 6), 3, 1, 0),
-    new PlatformConfig(new GridPoint2(27, 8), 3, 1, 10),
-    new PlatformConfig(new GridPoint2(23, 10), 3, 1, 2),
-    new PlatformConfig(new GridPoint2(14, 11), 3, 1, 0),
-    new PlatformConfig(new GridPoint2(9, 13), 3, 1, 0),
-
-    // second half
-    new PlatformConfig(new GridPoint2(56, 22), 3, 1, 1),
-    new PlatformConfig(new GridPoint2(60, 23), 3, 1, 1),
-    new PlatformConfig(new GridPoint2(64, 22), 3, 1, 1),
-  };
-
-  private static final PlatformConfig[] floors = {
-    // borders
-    new PlatformConfig(new GridPoint2(0, 0), 100, 1, 0),
-    new PlatformConfig(new GridPoint2(0, 22), 50, 5, 0),
-    new PlatformConfig(new GridPoint2(50, 25), 40, 5, 1),
-    new PlatformConfig(new GridPoint2(0, 0), 1, 30, 1),
-    new PlatformConfig(new GridPoint2(90, 0), 1, 30, 1),
-
-    // first half
-    new PlatformConfig(new GridPoint2(0, 0), 3, 3, 0),
-    new PlatformConfig(new GridPoint2(13, 0), 12, 5, 2),
-    new PlatformConfig(new GridPoint2(40, 0), 10, 15, 0),
-    new PlatformConfig(new GridPoint2(40, 15), 10, 2, 8),
-    new PlatformConfig(new GridPoint2(18, 15), 6, 2, 9),
-    new PlatformConfig(new GridPoint2(0, 13), 8, 3, 2),
-    new PlatformConfig(new GridPoint2(0, 16), 1, 5, 0),
-    new PlatformConfig(new GridPoint2(0, 21), 17, 1, 0),
-    new PlatformConfig(new GridPoint2(28, 19), 4, 1, 0),
-    new PlatformConfig(new GridPoint2(35, 21), 15, 1, 0),
-
-    // second half
-    new PlatformConfig(new GridPoint2(53, 13), 3, 4, 0),
-    new PlatformConfig(new GridPoint2(56, 15), 3, 1, 0),
-    new PlatformConfig(new GridPoint2(56, 16), 1, 3, 2),
-    new PlatformConfig(new GridPoint2(53, 7), 3, 4, 0),
-    new PlatformConfig(new GridPoint2(56, 10), 4, 1, 0),
-    new PlatformConfig(new GridPoint2(63, 0), 1, 18, 0),
-    new PlatformConfig(new GridPoint2(59, 18), 10, 1, 0),
-    new PlatformConfig(new GridPoint2(68, 15), 3, 1, 0),
-    new PlatformConfig(new GridPoint2(71, 15), 1, 6, 0),
-    new PlatformConfig(new GridPoint2(64, 15), 1, 1, 0),
-    new PlatformConfig(new GridPoint2(68, 11), 1, 3, 0),
-    new PlatformConfig(new GridPoint2(68, 11), 3, 1, 0),
-    new PlatformConfig(new GridPoint2(75, 15), 3, 1, 8),
-    new PlatformConfig(new GridPoint2(75, 12), 1, 3, 8),
-    new PlatformConfig(new GridPoint2(78, 0), 13, 17, 0),
-  };
-
-  private static final GridPoint2[] spikes = {
-    new GridPoint2(10, 2), new GridPoint2(20, 2), new GridPoint2(35, 2)
-  };
 
   private static final GridPoint2[] skeletonWarriorSpawnLocations =
       new GridPoint2[] {
@@ -102,18 +34,6 @@ public class TutorialGameArea extends GameArea {
       new GridPoint2[] {
         new GridPoint2(60, 1), new GridPoint2(57, 10),
       };
-
-  private static final GridPoint2 PLAYER_SPAWN = new GridPoint2(1, 4);
-  private static final GridPoint2 ROPE_ARROW_SPAWN = new GridPoint2(2, 4);
-  private static final GridPoint2 STANDARD_ARROW_SPAWN = new GridPoint2(4, 4);
-  private static final GridPoint2 FIRE_ARROW_SPAWN = new GridPoint2(6, 4);
-  private static final GridPoint2 COLD_ARROW_SPAWN = new GridPoint2(8, 4);
-  private static final GridPoint2 HEALTH_POTION_SPAWN = new GridPoint2(10, 4);
-
-  private static final int STANDARD_ARROW_QUANTITY = 5;
-  private static final int FIRE_ARROW_QUANTITY = 5;
-  private static final int COLD_ARROW_QUANTITY = 5;
-  private static final int HEALTH_POTION_QUANTITY = 3;
 
   private static final float WALL_WIDTH = 0.1f;
 
@@ -184,6 +104,7 @@ public class TutorialGameArea extends GameArea {
   public TutorialGameArea(TerrainFactory terrainFactory, CameraComponent camera) {
     super(camera);
 
+    config = new LevelTutorialConfig();
     this.terrainFactory = terrainFactory;
     this.camera = camera;
   }
@@ -191,40 +112,23 @@ public class TutorialGameArea extends GameArea {
   /** Create the game area, including terrain, background, platforms and a player. */
   @Override
   public void create() {
-
     loadAssets();
-
     displayUI();
 
     spawnTerrain();
-
     spawnBackground();
-
-    // spawnTrees();
-
-    spawnPlatforms();
-    spawnFloors();
-    spawnSpikes();
-
+    spawnConfigEntities();
     player = spawnPlayer();
-    spawnItems(); // test items
-    spawnWinCondition();
     spawnSkeletonArcher();
     spawnSkeletonWarrior();
     // spawnTestWinCondition(); // Temporary test win condition near player spawn for quick testing
-
-    // spawnGhosts();
-    // spawnGhostKing();
 
     // playMusic();
   }
 
   private void displayUI() {
-
     Entity ui = new Entity();
-
     ui.addComponent(new GameAreaDisplay("Tutorial"));
-
     spawnEntity(ui);
   }
 
@@ -244,7 +148,6 @@ public class TutorialGameArea extends GameArea {
    * the subtle effect you originally wanted.
    */
   private void spawnBackground() {
-
     BackgroundRenderComponent backgroundComponent = new BackgroundRenderComponent(camera);
 
     // Complete original background image
@@ -254,9 +157,7 @@ public class TutorialGameArea extends GameArea {
     // Create the background entity.
     Entity background = new Entity().addComponent(backgroundComponent);
 
-    /*
-     * Position the background in the game world.
-     */
+    // Position the background in the game world.
     background.setPosition(-20f, -10f);
 
     spawnEntity(background);
@@ -266,34 +167,16 @@ public class TutorialGameArea extends GameArea {
 
     // Background terrain
     terrain = terrainFactory.createTerrain(TerrainType.BACKGROUND_DESERT);
-
     spawnEntity(new Entity().addComponent(terrain));
 
     // Terrain walls
     float tileSize = terrain.getTileSize();
-
     GridPoint2 tileBounds = terrain.getMapBounds(0);
-
     Vector2 worldBounds = new Vector2(tileBounds.x * tileSize, tileBounds.y * tileSize);
-
-    spawnMovingPlatforms();
 
     // Left wall
     spawnEntityAt(
         ObstacleFactory.createWall(WALL_WIDTH, worldBounds.y), GridPoint2Utils.ZERO, false, false);
-
-    // Right wall
-    /*
-    spawnEntityAt(
-        ObstacleFactory.createWall(
-            WALL_WIDTH,
-            worldBounds.y
-        ),
-        new GridPoint2(tileBounds.x, 0),
-        false,
-        false
-    );
-    */
 
     // Top wall
     spawnEntityAt(
@@ -307,75 +190,6 @@ public class TutorialGameArea extends GameArea {
         ObstacleFactory.createWall(worldBounds.x, WALL_WIDTH), GridPoint2Utils.ZERO, false, false);
   }
 
-  private void spawnMovingPlatforms() {
-
-    Entity movingPlatform =
-        ObstacleFactory.createMovingPlatform(
-            0, new Vector2(9, 15), new Vector2(14, 15), new Vector2(3, 0));
-    movingPlatform.setScale(2, 1);
-    spawnEntityAt(movingPlatform, new GridPoint2(9, 15), false, false);
-
-    Entity movingPlatform2 =
-        ObstacleFactory.createMovingPlatform(
-            0, new Vector2(25, 15), new Vector2(36, 15), new Vector2(3, 0));
-    movingPlatform2.setScale(2, 1);
-    spawnEntityAt(movingPlatform2, new GridPoint2(25, 15), false, false);
-
-    Entity movingPlatform3 =
-        ObstacleFactory.createMovingPlatform(
-            10, new Vector2(61, 2), new Vector2(61, 10), new Vector2(0, 3));
-    spawnEntityAt(movingPlatform3, new GridPoint2(61, 10), false, false);
-
-    Entity movingPlatform4 =
-        ObstacleFactory.createMovingPlatform(
-            0, new Vector2(60, 12), new Vector2(60, 15), new Vector2(0, 3));
-    spawnEntityAt(movingPlatform4, new GridPoint2(60, 12), false, false);
-
-    Entity movingPlatform5 =
-        ObstacleFactory.createMovingPlatform(
-            10, new Vector2(72, 2), new Vector2(72, 12), new Vector2(0, 3));
-    spawnEntityAt(movingPlatform5, new GridPoint2(75, 2), false, false);
-  }
-
-  private void spawnPlatforms() {
-
-    for (PlatformConfig config : platforms) {
-      Entity platform = ObstacleFactory.createPlatform(config.grappleSides);
-
-      platform.setScale(config.width, config.height);
-
-      spawnEntityAt(platform, config.position, false, false);
-    }
-  }
-
-  private void spawnFloors() {
-
-    for (PlatformConfig config : floors) {
-      Entity platform = ObstacleFactory.createFloor(config.grappleSides);
-
-      platform.setScale(config.width, config.height);
-
-      spawnEntityAt(platform, config.position, false, false);
-    }
-  }
-
-  private void spawnSpikes() {
-    // 1. Upper-Left Block Top Surface (Y = 17)
-    for (int x = 53; x <= 55; x++) {
-      spawnEntityAt(ObstacleFactory.createSpike(), new GridPoint2(x, 17), true, true);
-    }
-
-    // 2. Overhead T-Bar Top Surface (Y = 19)
-    for (int x = 59; x <= 68; x++) {
-      spawnEntityAt(ObstacleFactory.createSpike(), new GridPoint2(x, 19), true, true);
-    }
-
-    // 3. Ground Pit Surface (Y = 1)
-    for (int x = 60; x <= 62; x++) {
-      spawnEntityAt(ObstacleFactory.createSpike(), new GridPoint2(x, 1), true, true);
-    }
-  }
-
   private Entity spawnPlayer() {
 
     Entity newPlayer = PlayerFactory.createPlayer();
@@ -385,14 +199,9 @@ public class TutorialGameArea extends GameArea {
     if (input != null) {
       input.setCameraComponent(cameraComponent);
     }
-    spawnEntityAt(newPlayer, PLAYER_SPAWN, true, true);
+    spawnEntityAt(newPlayer, config.getPlayerSpawn(), true, true);
 
     return newPlayer;
-  }
-
-  private void spawnWinCondition() {
-    Entity winCon = ObstacleFactory.createWinConEntity();
-    spawnEntityAt(winCon, new GridPoint2(80, 18), true, true);
   }
 
   // Temporary test win condition near player spawn for quick testing
@@ -400,22 +209,6 @@ public class TutorialGameArea extends GameArea {
     // Temporary test win condition near player spawn for quick testing
     Entity testWinCon = ObstacleFactory.createWinConEntity();
     spawnEntityAt(testWinCon, new GridPoint2(3, 4), true, true);
-  }
-
-  private void spawnGhosts() {
-
-    GridPoint2 minPos = new GridPoint2(0, 0);
-
-    GridPoint2 maxPos = terrain.getMapBounds(0).sub(2, 2);
-
-    for (int i = 0; i < NUM_GHOSTS; i++) {
-
-      GridPoint2 randomPos = RandomUtils.random(minPos, maxPos);
-
-      Entity ghost = NPCFactory.createGhost(player);
-
-      spawnEntityAt(ghost, randomPos, true, true);
-    }
   }
 
   private void spawnSkeletonWarrior() {
@@ -432,19 +225,6 @@ public class TutorialGameArea extends GameArea {
     }
   }
 
-  private void spawnGhostKing() {
-
-    GridPoint2 minPos = new GridPoint2(0, 0);
-
-    GridPoint2 maxPos = terrain.getMapBounds(0).sub(2, 2);
-
-    GridPoint2 randomPos = RandomUtils.random(minPos, maxPos);
-
-    Entity ghostKing = NPCFactory.createGhostKing(player);
-
-    spawnEntityAt(ghostKing, randomPos, true, true);
-  }
-
   /** Plays the background music. */
   private void playMusic() {
 
@@ -457,38 +237,27 @@ public class TutorialGameArea extends GameArea {
 
   /** Loads all assets. */
   private void loadAssets() {
-
     logger.debug("Loading assets");
 
     ResourceService resourceService = ServiceLocator.getResourceService();
-
     resourceService.loadTextures(forestTextures);
-
     resourceService.loadTextureAtlases(forestTextureAtlases);
-
     resourceService.loadSounds(forestSounds);
-
     resourceService.loadMusic(forestMusic);
 
     while (!resourceService.loadForMillis(10)) {
-
       logger.info("Loading... {}%", resourceService.getProgress());
     }
   }
 
   /** Unloads all assets. */
   private void unloadAssets() {
-
     logger.debug("Unloading assets");
 
     ResourceService resourceService = ServiceLocator.getResourceService();
-
     resourceService.unloadAssets(forestTextures);
-
     resourceService.unloadAssets(forestTextureAtlases);
-
     resourceService.unloadAssets(forestSounds);
-
     resourceService.unloadAssets(forestMusic);
   }
 
@@ -499,29 +268,8 @@ public class TutorialGameArea extends GameArea {
   /** Dispose of the game area. */
   @Override
   public void dispose() {
-
     super.dispose();
-
     ServiceLocator.getResourceService().getAsset(backgroundMusic, Music.class).stop();
-
     this.unloadAssets();
-  }
-
-  /** generate items */
-  private void spawnItems() {
-    spawnEntityAt(ItemFactory.createRopeArrow(), ROPE_ARROW_SPAWN, true, false);
-
-    spawnEntityAt(
-        ItemFactory.createStandardArrow(STANDARD_ARROW_QUANTITY),
-        STANDARD_ARROW_SPAWN,
-        true,
-        false);
-
-    spawnEntityAt(
-        ItemFactory.createHealthPotion(HEALTH_POTION_QUANTITY), HEALTH_POTION_SPAWN, true, false);
-
-    spawnEntityAt(ItemFactory.createFireArrow(FIRE_ARROW_QUANTITY), FIRE_ARROW_SPAWN, true, false);
-
-    spawnEntityAt(ItemFactory.createColdArrow(COLD_ARROW_QUANTITY), COLD_ARROW_SPAWN, true, false);
   }
 }
