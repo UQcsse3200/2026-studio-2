8d671046ed94a4632a2fbcd863589b6cfdd39f9e
s45855377 <c.somerfield@student.uq.edu.au>
2026-09-10 09:17:47 +1000

    Added placeholder configuration to test ledge collision

--- source/core/src/main/com/csse3200/game/areas/terrain/configs/levelconfigs/Level2Config.java
+++ source/core/src/main/com/csse3200/game/areas/terrain/configs/levelconfigs/Level2Config.java
@@ -2,10 +2,15 @@ package com.csse3200.game.areas.terrain.configs.levelconfigs;
 
 import com.badlogic.gdx.math.GridPoint2;
 import com.csse3200.game.areas.terrain.configs.LevelConfig;
+import com.csse3200.game.areas.terrain.configs.PlatformConfig;
 
 public class Level2Config extends LevelConfig {
   public Level2Config() {
     playerSpawn = new GridPoint2(1, 4);
     winConditionSpawn = new GridPoint2(80, 18);
+
+    ledges = new PlatformConfig[] {new PlatformConfig(new GridPoint2(1, 4), 3, 1, 0)};
+
+    floors = new PlatformConfig[] {new PlatformConfig(new GridPoint2(1, 1), 3, 1, 0)};
   }
 }
