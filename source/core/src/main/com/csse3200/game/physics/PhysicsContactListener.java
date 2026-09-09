5851f192f204bcf135c4d57082d4ee681d3a8a13
s45855377 <c.somerfield@student.uq.edu.au>
2026-09-10 09:18:53 +1000

    Added functionality to preSolve to turn off contact between the player and ledges when the player is moving up through the ledge

--- source/core/src/main/com/csse3200/game/physics/PhysicsContactListener.java
+++ source/core/src/main/com/csse3200/game/physics/PhysicsContactListener.java
@@ -1,6 +1,8 @@
 package com.csse3200.game.physics;
 
 import com.badlogic.gdx.physics.box2d.*;
+import com.csse3200.game.components.level.LedgeComponent;
+import com.csse3200.game.entities.Entity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,7 +33,40 @@ public class PhysicsContactListener implements ContactListener {
 
   @Override
   public void preSolve(Contact contact, Manifold oldManifold) {
-    // Nothing to do before resolving contact
+    // determine what has made contact
+    Fixture fixtureA = contact.getFixtureA();
+    Fixture fixtureB = contact.getFixtureB();
+
+    // get BodyUserData for each entity that made contact and ensure they're not null
+    BodyUserData dataA = (BodyUserData) fixtureA.getBody().getUserData();
+    BodyUserData dataB = (BodyUserData) fixtureB.getBody().getUserData();
+    if (dataA == null || dataB == null) {
+      return;
+    }
+
+    // get entity reference stored in BodyUserData and ensure they're not null
+    Entity entityA = dataA.entity;
+    Entity entityB = dataB.entity;
+    if (entityA == null || entityB == null) {
+      return;
+    }
+
+    // determine if one of the fixtures involved in the collision is a ledge
+    boolean entityALedge = entityA.getComponent(LedgeComponent.class) != null;
+    boolean entityBLedge = entityB.getComponent(LedgeComponent.class) != null;
+    if (!entityALedge && !entityBLedge) {
+      return;
+    }
+
+    // if we found a ledge, we need to check if the player is attempting to move through the bottom
+    // of the ledge fixture. if so, we disable contact for this collision
+    Fixture playerFixture = entityALedge ? fixtureB : fixtureA;
+
+    // determine velocity to ensure the player is moving up at time of collision
+    Body playerBody = playerFixture.getBody();
+    if (playerBody.getLinearVelocity().y > 0) {
+      contact.setEnabled(false);
+    }
   }
 
   @Override
