059d691e78a175137f607a2337f8c9cc7831657d
nedlonghurst <s4963211@student.uq.edu.au>
2026-09-10 15:44:40 +1000

    Removed obsolete function from PhysicsMovementComponent.java

--- source/core/src/main/com/csse3200/game/physics/components/PhysicsMovementComponent.java
+++ source/core/src/main/com/csse3200/game/physics/components/PhysicsMovementComponent.java
@@ -102,9 +102,4 @@ public class PhysicsMovementComponent extends Component implements MovementContr
     }
     body.applyLinearImpulse(impulse, body.getWorldCenter(), true);
   }
-
-  private Vector2 getDirection() {
-    // Move towards targetPosition based on our current position
-    return targetPosition.cpy().sub(entity.getPosition()).nor();
-  }
 }
