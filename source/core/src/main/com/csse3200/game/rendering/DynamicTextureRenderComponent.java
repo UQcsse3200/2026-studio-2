ae7b5510a46fce04bd42960465be12a86e3c2a03
s45855377 <c.somerfield@student.uq.edu.au>
2026-09-13 09:03:21 +1000

    Added the ability to rotate textures

--- source/core/src/main/com/csse3200/game/rendering/DynamicTextureRenderComponent.java
+++ source/core/src/main/com/csse3200/game/rendering/DynamicTextureRenderComponent.java
@@ -2,26 +2,39 @@ package com.csse3200.game.rendering;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
+import com.csse3200.game.components.level.RotatableMapComponent;
 import com.csse3200.game.services.ServiceLocator;
 
 public class DynamicTextureRenderComponent extends RenderComponent {
   private Texture texture;
+  private TextureRegion textureRegion;
+  private RotatableMapComponent rotateComponent;
+  private float rotation;
 
   public DynamicTextureRenderComponent(String texturePath) {
     setTexture(texturePath);
   }
 
   public DynamicTextureRenderComponent(Texture texture) {
-    this.texture = texture;
+    setTexture(texture);
+  }
+
+  @Override
+  public void create() {
+    super.create();
+    rotateComponent = entity.getComponent(RotatableMapComponent.class);
   }
 
   public void setTexture(String texturePath) {
     texture = ServiceLocator.getResourceService().getAsset(texturePath, Texture.class);
+    textureRegion = new TextureRegion(texture);
   }
 
   public void setTexture(Texture texture) {
     this.texture = texture;
+    textureRegion = new TextureRegion(texture);
   }
 
   public void scaleEntity() {
@@ -32,6 +45,17 @@ public class DynamicTextureRenderComponent extends RenderComponent {
   protected void draw(SpriteBatch batch) {
     Vector2 position = entity.getPosition();
     Vector2 scale = entity.getScale();
-    batch.draw(texture, position.x, position.y, scale.x, scale.y);
+    rotation = rotateComponent.getRotation();
+    batch.draw(
+        textureRegion,
+        position.x,
+        position.y,
+        scale.x / 2f,
+        scale.y / 2f,
+        scale.x,
+        scale.y,
+        1f,
+        1f,
+        rotation);
   }
 }
