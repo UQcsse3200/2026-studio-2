e4ddb4d9e6cc2ff91eba538c51e80735a9e30833
TristanUniCoding <t.sheedy@student.uq.edu.au>
2026-09-13 22:30:42 +1000

    Unit tests

--- source/core/src/test/com/csse3200/game/rendering/PlayerAtlasTest.java
+++ source/core/src/test/com/csse3200/game/rendering/PlayerAtlasTest.java
@@ -1,25 +1,63 @@
 package com.csse3200.game.rendering;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.csse3200.game.extensions.GameExtension;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 /** Verifies images/player.atlas parses correctly and exposes the regions the player uses. */
 @ExtendWith(GameExtension.class)
 class PlayerAtlasTest {
+  private TextureAtlas atlas;
+
+  @BeforeEach
+  void beforeEach() {
+    atlas = new TextureAtlas(Gdx.files.internal("images/player.atlas"));
+  }
+
+  @AfterEach
+  void afterEach() {
+    atlas.dispose();
+  }
+
   @Test
-  void shouldParseExpectedRegions() {
-    //    TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("images/player.atlas"));
-    //
-    //    assertNotNull(atlas.findRegion("default"));
-    //    assertNotNull(atlas.findRegion("idle"));
-    //    assertEquals(26, atlas.findRegion("default").getRegionWidth());
-    //    assertEquals(82, atlas.findRegion("default").getRegionHeight());
-    //    assertEquals(16, atlas.findRegions("idle").size);
-    //    assertEquals(8, atlas.findRegions("walk").size);
-    //    assertEquals(12, atlas.findRegions("sprint").size);
-    //    assertEquals(16, atlas.findRegions("jump").size);
-    //
-    //    atlas.dispose();
+  void shouldExposeIdleAnimation() {
+    assertEquals(16, atlas.findRegions("idle").size);
+  }
+
+  @Test
+  void shouldExposeWalkAnimation() {
+    assertEquals(8, atlas.findRegions("walk").size);
+  }
+
+  @Test
+  void shouldExposeSprintAnimation() {
+    assertEquals(12, atlas.findRegions("sprint").size);
+  }
+
+  @Test
+  void shouldExposeJumpAnimation() {
+    assertEquals(11, atlas.findRegions("jump").size);
+  }
+
+  @Test
+  void shouldExposeHurtAnimation() {
+    assertEquals(21, atlas.findRegions("hurt").size);
+  }
+
+  @Test
+  void shouldExposeDeathAnimation() {
+    assertEquals(16, atlas.findRegions("death").size);
+  }
+
+  @Test
+  void shouldExposeSleepAnimation() {
+    assertEquals(16, atlas.findRegions("sleep").size);
   }
 }
