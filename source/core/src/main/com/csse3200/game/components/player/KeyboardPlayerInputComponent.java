2fa696c1a09076c1071cc12c43705eb2722a37a3
Lovien <lovien0322@gmail.com>
2026-09-15 23:20:15 +1000

    Remove unused Input import so Java format CI can pass.

--- source/core/src/main/com/csse3200/game/components/player/KeyboardPlayerInputComponent.java
+++ source/core/src/main/com/csse3200/game/components/player/KeyboardPlayerInputComponent.java
@@ -1,7 +1,6 @@
 package com.csse3200.game.components.player;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Input.Buttons;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
